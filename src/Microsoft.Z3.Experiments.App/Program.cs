0c1206265fbc4142ba7476dffb3cf2ac4ca9c6d4
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-10-24 18:33:45 +0300

    Add experiments

---
+++ src/Microsoft.Z3.Experiments.App/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Microsoft.Z3.Experiments.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            using (Context ctx = new Context())
+            {
+                Console.WriteLine(CheckSat(ctx, CreateExprContradiction(ctx)));
+
+                Console.WriteLine(CheckSat(ctx, CreateExprIsEmpty(ctx)));
+
+                ctx.Dispose();
+            }
+        }
+
+        private static BoolExpr CreateExprContradiction(Context ctx)
+        {
+            var varA = ctx.MkConst(ctx.MkSymbol("a"), ctx.StringSort);
+            return ctx.MkAnd(
+                ctx.MkEq(varA, ctx.MkString("1")),
+                ctx.MkNot(
+                    ctx.MkEq(varA, ctx.MkString("1"))));
+        }
+
+        private static BoolExpr CreateExprIsEmpty(Context ctx)
+        {
+            var varA = ctx.MkConst(ctx.MkSymbol("a"), ctx.StringSort);
+            return ctx.MkAnd(
+                ctx.MkOr(
+                    ctx.MkEq(varA, ctx.MkString("")),
+                    ctx.MkEq(varA, ctx.MkString("NULL"))),
+                ctx.MkNot(ctx.MkEq(varA, ctx.MkString("NA"))));
+        }
+
+        private static Status CheckSat(Context ctx, BoolExpr eq)
+        {
+            var solver = ctx.MkSolver();
+            solver.Assert(eq);
+            return solver.Check();
+        }
+    }
+}
